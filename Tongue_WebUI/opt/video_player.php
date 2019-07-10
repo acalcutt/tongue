ffdc96ca544829697527835fdf97357c052dd77b
acalcutt <acalcutt@techidiots.net>
2019-07-10 11:04:17 -0400

    mostly files from master branch + flow player

--- Tongue_WebUI/opt/video_player.php
+++ Tongue_WebUI/opt/video_player.php
@@ -6,7 +6,7 @@
  * Time: 10:03 PM
  */
 require "init.php";
-$res = $conn->query("SELECT `feed`, `feed_server` FROM `tongue`.`feeds` WHERE `in_use` != 1 LIMIT 1");
+$res = $conn->query("SELECT `feed`, `feed_server` FROM `tongue`.`feeds` WHERE `in_use` != 1 LIMIT 1\r\n");
 $feeds = $res->fetch(2);
 var_dump($feeds);
 $feed = $feeds['feed'];
@@ -14,15 +14,17 @@ $exp = explode(".", $feed);
 $len = strlen($exp[0]);
 $int = (int)$exp[0][$len-1];
 $feed_server = $feeds['feed_server'];
-$res = $conn->prepare("INSERT INTO `tongue`.`waiting` (`video_id`, `table`, `feed`, `feed_server`, `seek`) VALUES(?, ?, ?, ?, ?)");
-
+$video_id = $_GET['video'];
+$table = $_GET['table'];
 $seek = "00:00:00";
+$res = $conn->prepare("INSERT INTO `tongue`.`waiting` (`video_id`, `table`, `feed`, `feed_server`, `seek`) VALUES(?, ?, ?, ?, ?)");
+echo "INSERT INTO `tongue`.`waiting` (`video_id`, `table`, `feed`, `feed_server`, `seek`) VALUES($video_id, $table, $feed, $feed_server, $seek)";
 
-$res->bindParam(1, $_GET['video_id'], PDO::PARAM_INT);
-$res->bindParam(3, $_GET['table'], PDO::PARAM_STR);
-$res->bindParam(2, $feed, PDO::PARAM_STR);
-$res->bindParam(3, $feed_server, PDO::PARAM_STR);
-$res->bindParam(4, $seek, PDO::PARAM_STR);
+$res->bindParam(1, $video_id, PDO::PARAM_INT);
+$res->bindParam(2, $table, PDO::PARAM_STR);
+$res->bindParam(3, $feed, PDO::PARAM_STR);
+$res->bindParam(4, $feed_server, PDO::PARAM_STR);
+$res->bindParam(5, $seek, PDO::PARAM_STR);
 $res->execute();
 sleep(1);
 $stream_source = "http://".$feed_server."/stream".$int.".webm";
